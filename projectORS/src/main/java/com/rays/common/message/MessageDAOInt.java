ac2322028b6aa82785efca263d37c94a09bff8fc
rvpatidar12 <105790203+rvpatidar12@users.noreply.github.com>
2023-04-12 20:19:01 +0530

    java

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Ravi Patidar
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
